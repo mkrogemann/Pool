81763235208d0d30553bbf57baf2f50b820995ba
Markus Krogemann <markus@krogemann.de>
2019-04-21 11:25:11 +0200

    Handle match setup for races and endless matches

--- app/src/main/java/de/krogemann/pool/TeamsActivity.kt
+++ app/src/main/java/de/krogemann/pool/TeamsActivity.kt
@@ -3,6 +3,8 @@ package de.krogemann.pool
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import de.krogemann.pool.match.Match
 import kotlinx.android.synthetic.main.activity_teams.*
 
 class TeamsActivity : AppCompatActivity() {
@@ -11,9 +13,42 @@ class TeamsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
 
-        start_match_button.setOnClickListener {
-            // create match and switch to match activity
-            startActivity(Intent(this, MatchActivity::class.java))
+        race_checkbox.setOnClickListener {
+            setRaceVisibility()
         }
+
+        start_match_button.setOnClickListener {
+            val bundle = Bundle()
+            val intent = Intent(this, MatchActivity::class.java)
+            val match = setupMatch()
+            bundle.putParcelable("MATCH", match)
+            intent.putExtras(bundle)
+
+            startActivity(intent)
+        }
+    }
+
+    private fun setRaceVisibility() {
+        if (race_checkbox.isChecked) {
+            race_to.visibility = View.VISIBLE
+            race_to_label.visibility = View.VISIBLE
+        } else {
+            race_to.visibility = View.INVISIBLE
+            race_to_label.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setupMatch(): Match {
+        val match = Match(
+            teamOne = team_one.text.toString(),
+            teamTwo = team_two.text.toString(),
+            raceTo = if (race_checkbox.isChecked) {
+                race_to.text.toString().toInt()
+            } else {
+                null
+            },
+            winnerBreak = winner_break.isChecked
+        )
+        return match
     }
 }
