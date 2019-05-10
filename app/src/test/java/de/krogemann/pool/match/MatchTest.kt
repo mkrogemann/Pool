2a00a6df7f5a13d70ef8bd7c2b8cb7138c2e4012
Markus Krogemann <markus@krogemann.de>
2019-05-10 18:23:05 +0200

    Fix typo in tested team name value

--- app/src/test/java/de/krogemann/pool/match/MatchTest.kt
+++ app/src/test/java/de/krogemann/pool/match/MatchTest.kt
@@ -8,10 +8,10 @@ class MatchTest {
 
     @Test
     fun constructs_with_sensible_defaults() {
-        val match = Match(teamOne = "tean_one", teamTwo = "team_two",
+        val match = Match(teamOne = "team_one", teamTwo = "team_two",
             raceTo = null, winnerBreak = false)
 
-        assertThat(match.teamOne, `is`("tean_one"))
+        assertThat(match.teamOne, `is`("team_one"))
         assertThat(match.teamTwo, `is`("team_two"))
         assertThat(match.teamOneWins, `is`(0))
         assertThat(match.teamTwoWins, `is`(0))
