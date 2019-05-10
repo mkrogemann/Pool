5f6567e65542d8c5dd2a9318798b8e804e280a17
Markus Krogemann <markus@krogemann.de>
2019-05-10 17:04:55 +0200

    Pass match data between activities as parcelable

--- app/src/main/java/de/krogemann/pool/MatchActivity.kt
+++ app/src/main/java/de/krogemann/pool/MatchActivity.kt
@@ -2,11 +2,17 @@ package de.krogemann.pool
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import de.krogemann.pool.match.Match
+import kotlinx.android.synthetic.main.activity_match.*
 
 class MatchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
+        val parcelable = intent.extras.getParcelable<Match>("MATCH")
+        val (teamOne, teamTwo, _, _, _, _, _) = parcelable
+        team_one_wins.text = "$teamOne wins"
+        team_two_wins.text = "$teamTwo wins"
     }
 }
